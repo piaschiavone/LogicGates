eacdf369f21bb35db15e6cf6bb1db33f71338933
piaschiavone <apuntespia0@gmail.com>
2026-09-22 10:03:55 -0300

    git

--- test/LibraryTests/VariableValue.cs
+++ test/LibraryTests/VariableValue.cs
@@ -1,8 +1,8 @@
-public class LogicConstant : ILogicValue
+public class VariableValue : ILogicValue
 {
     private bool value;
 
-    public LogicConstant(bool value)
+    public VariableValue(bool value)
     {
         this.value = value;
     }
